9194b330242ed91f47b701037f2fc5881e72d66f
Mateusz <mateusz.skiba114@gmail.com>
2026-01-11 12:29:47 +0100

    first commit

---
+++ server/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+app = FastAPI()
+
+api_router = APIRouter(prefix="/api")
+
+@api_router.get("/")
+def root():
+    return {"data": "Hello World!"}
+
+@api_router.get("/add")
+def add(a: float, b: float):
+    return {"data": a + b}
+
+@api_router.get("/health")
+def health_check():
+    return {"status": "ok", "version": os.getenv("API_VERSION", "1.0.0")}
+
+app.include_router(api_router)
